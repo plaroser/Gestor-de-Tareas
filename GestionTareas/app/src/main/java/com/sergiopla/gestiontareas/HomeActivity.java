dc95cb55d3b03079813ab6ad58110e07fc6be81b
MSI-SOBREMESA <git@bitbucket.org:plaroser/pruebaandroid.git>
2017-11-23 17:46:05 +0100

    Fragments creados de la actividad home

--- GestionTareas/app/src/main/java/com/sergiopla/gestiontareas/HomeActivity.java
+++ GestionTareas/app/src/main/java/com/sergiopla/gestiontareas/HomeActivity.java
@@ -1,23 +1,23 @@
 package com.sergiopla.gestiontareas;
 
-import android.support.design.widget.TabLayout;
+import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
-import android.os.Bundle;
-import android.view.LayoutInflater;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
+import android.widget.Toast;
 
-import android.widget.TextView;
+import com.sergiopla.gestiontareas.fragments.HomeFragment;
+import com.sergiopla.gestiontareas.fragments.SettingsFragment;
+import com.sergiopla.gestiontareas.fragments.TareasFragment;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -41,8 +41,8 @@ public class HomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
         // Create the adapter that will return a fragment for each of the three
         // primary sections of the activity.
         mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
@@ -90,44 +90,9 @@ public class HomeActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-        /**
-         * The fragment argument representing the section number for this
-         * fragment.
-         */
-        private static final String ARG_SECTION_NUMBER = "section_number";
-
-        public PlaceholderFragment() {
-        }
-
-        /**
-         * Returns a new instance of this fragment for the given section
-         * number.
-         */
-        public static PlaceholderFragment newInstance(int sectionNumber) {
-            PlaceholderFragment fragment = new PlaceholderFragment();
-            Bundle args = new Bundle();
-            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-            fragment.setArguments(args);
-            return fragment;
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_home, container, false);
-            TextView textView = (TextView) rootView.findViewById(R.id.section_label);
-            textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
-            return rootView;
-        }
-    }
 
     /**
-     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
-     * one of the sections/tabs/pages.
+     * Se encarga de manejar los fragment que se muestran
      */
     public class SectionsPagerAdapter extends FragmentPagerAdapter {
 
@@ -135,15 +100,30 @@ public class HomeActivity extends AppCompatActivity {
             super(fm);
         }
 
+        /**
+         * Se encarga de cargar el fragmen que toca en cada momento
+         *
+         * @param position posicion a cargar
+         * @return fragment cargado
+         */
         @Override
         public Fragment getItem(int position) {
-            // getItem is called to instantiate the fragment for the given page.
-            // Return a PlaceholderFragment (defined as a static inner class below).
-            return PlaceholderFragment.newInstance(position + 1);
+            switch (position) {
+                case 1:
+                    return SettingsFragment.newInstance();
+                case 2:
+                    return HomeFragment.newInstance();
+                case 3:
+                    return TareasFragment.newInstance();
+                default:
+                    Toast.makeText(getApplicationContext(), "ERROR en la numeracion de los fragment", Toast.LENGTH_LONG).show();
+                    return HomeFragment.newInstance();
+            }
         }
 
         @Override
         public int getCount() {
+
             // Show 3 total pages.
             return 3;
         }
