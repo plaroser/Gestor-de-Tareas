3599d4efe1e6c1fc6212ec090b6dac51fb9707ea
DESKTOP-TNPQ1R7\alber <albertotrabu@gmail.com>
2017-12-04 14:28:22 +0100

    Correccion en los edit text y registro completado

--- GestionTareas/app/src/main/java/com/sergiopla/gestiontareas/HomeActivity.java
+++ GestionTareas/app/src/main/java/com/sergiopla/gestiontareas/HomeActivity.java
@@ -31,7 +31,6 @@ public class HomeActivity extends AppCompatActivity
     private ViewPager mViewPager;
 
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
--- GestionTareas/app/src/main/java/com/sergiopla/gestiontareas/Registro.java
+++ GestionTareas/app/src/main/java/com/sergiopla/gestiontareas/Registro.java
@@ -3,28 +3,51 @@ package com.sergiopla.gestiontareas;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Patterns;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.regex.Pattern;
 
 public class Registro extends AppCompatActivity {
 
     Button crear;
+    EditText edtCorreo;
+    EditText edtContraseña;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
 
-        crear=(Button) findViewById(R.id.btnCrearCuenta);
+
+        edtCorreo = (EditText) findViewById(R.id.edtContraseña);
+        edtContraseña = (EditText) findViewById(R.id.edtContraseña);
+        crear = (Button) findViewById(R.id.btnCrearCuenta);
 
         crear.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent crear= new Intent(Registro.this, LoginActivity.class);
-                startActivity(crear);
+                Intent crear = new Intent(Registro.this, LoginActivity.class);
 
-                Toast.makeText(Registro.this, "Cuenta creada con exito", Toast.LENGTH_SHORT).show();
+                if (edtCorreo.getText().toString().contains("@")&& edtContraseña.getText().length()>4) {
+                    Toast.makeText(Registro.this, "Cuenta creada con exito", Toast.LENGTH_SHORT).show();
+                    startActivity(crear);
+
+
+                }else if(!Patterns.EMAIL_ADDRESS.matcher(edtCorreo.getText()).matches()){
+                    Toast.makeText(Registro.this, "Email incorrecto", Toast.LENGTH_SHORT).show();
+
+                }else {
+                    Toast.makeText(Registro.this, "Contraseña debe tener al menos 4 caracteres", Toast.LENGTH_SHORT).show();
+
+                }
+
+
             }
         });
     }
