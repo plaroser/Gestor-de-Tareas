acbf302d1301ae08aa0b6c034b5dd5688b411249
MSI-SOBREMESA <git@bitbucket.org:plaroser/pruebaandroid.git>
2017-12-11 18:24:42 +0100

    Clase HomeFragment.java documentada

--- GestionTareas/app/src/main/java/com/sergiopla/gestiontareas/fragments/HomeFragment.java
+++ GestionTareas/app/src/main/java/com/sergiopla/gestiontareas/fragments/HomeFragment.java
@@ -9,10 +9,8 @@ import android.view.ViewGroup;
 import android.widget.Button;
 
 import com.sergiopla.gestiontareas.CrearTarea;
-import com.sergiopla.gestiontareas.HomeActivity;
 import com.sergiopla.gestiontareas.ListarEquiposActivity;
 import com.sergiopla.gestiontareas.R;
-import com.sergiopla.gestiontareas.VerEquipoActivity;
 
 /**
  * Created by SergioPla on 11/23/17.
@@ -28,42 +26,43 @@ public class HomeFragment extends Fragment {
     Button crearTarea;
     Button verEquipos;
 
-    public HomeFragment() {
-    }
-
     /**
      * Returns a new instance of this fragment for the given section
      * number.
      */
     public static HomeFragment newInstance() {
         HomeFragment fragment = new HomeFragment();
-//        Bundle args = new Bundle();
-//        args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-//        fragment.setArguments(args);
+
         return fragment;
     }
 
+    /**
+     * Metodo que se ejecuta al crear dicha vista
+     *
+     * @param inflater           elemento que muestra esta vista
+     * @param container          vista que contiene este fragment
+     * @param savedInstanceState Bundle que puede contener informacion adicional
+     * @return Vista creada
+     */
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_home, container, false);
-//        TextView textView = (TextView) rootView.findViewById(R.id.section_label);
-//        textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
 
-        crearTarea=(Button) rootView.findViewById(R.id.btnCrearTarea);
+        crearTarea = (Button) rootView.findViewById(R.id.btnCrearTarea);
         crearTarea.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent crearTarea= new Intent (getActivity(), CrearTarea.class);
+                Intent crearTarea = new Intent(getActivity(), CrearTarea.class);
                 startActivity(crearTarea);
             }
         });
 
-        verEquipos=(Button) rootView.findViewById(R.id.btnVerEquipos);
+        verEquipos = (Button) rootView.findViewById(R.id.btnVerEquipos);
         verEquipos.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent verEquipos = new Intent (getActivity(),ListarEquiposActivity.class);
+                Intent verEquipos = new Intent(getActivity(), ListarEquiposActivity.class);
                 startActivity(verEquipos);
             }
         });
